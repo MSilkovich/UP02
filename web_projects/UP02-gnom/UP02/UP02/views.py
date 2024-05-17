2a188ad086537af29af5ad733c86dcf646e15b63
kidal <danalex2006@yandex.ru>
2024-05-17 11:10:57 +0300

    Починены вкладки (+-)

---
+++ web_projects/UP02-gnom/UP02/UP02/views.py
@@ -0,0 +1,49 @@
+"""
+Routes and views for the flask application.
+"""
+
+from datetime import datetime
+import os
+from flask import abort, render_template, render_template_string
+from UP02 import app
+from .code import create_chart
+
+@app.route('/')
+@app.route('/home')
+def home():
+    """Renders the home page."""
+    return render_template(
+        'index.html',
+        title='Home Page',
+        year=datetime.now().year,
+    )
+
+@app.route('/contact')
+def contact():
+    """Renders the contact page."""
+    return render_template(
+        'contact.html',
+        title='Contact',
+        year=datetime.now().year,
+        message='Your contact page.'
+    )
+
+@app.route('/about')
+def about():
+    """Renders the about page."""
+    return render_template(
+        'about.html',
+        title='About',
+        year=datetime.now().year,
+        message='Your application description page.'
+    )
+
+
+@app.route('/load-html/<file>/<tag>')
+def load_html(file, tag):
+    with open(f'UP02/templates/{file}', 'r') as f:
+        html = f.read()
+        if file == "comparison.html":
+            chart = create_chart()
+            return render_template_string(html, tag=tag, chart=chart)
+        return render_template_string(html, tag=tag)
